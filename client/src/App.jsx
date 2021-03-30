1e1b30fd5a361fc95a09d9a89bb6cc951685ee4f
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2021-03-30 20:59:36 +0100

    code formated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,120 +1,133 @@
-import IconButton from "@material-ui/core/IconButton"
-import TextField from "@material-ui/core/TextField"
-import AssignmentIcon from "@material-ui/icons/Assignment"
-import PhoneIcon from "@material-ui/icons/Phone"
-import Card from '@material-ui/core/Card'
-import Grid from '@material-ui/core/Grid'
-import React, { useEffect, useRef, useState } from "react"
-import { CopyToClipboard } from "react-copy-to-clipboard"
-import Peer from "simple-peer"
-import io from "socket.io-client"
+import IconButton from "@material-ui/core/IconButton";
+import TextField from "@material-ui/core/TextField";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import PhoneIcon from "@material-ui/icons/Phone";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import React, { useEffect, useRef, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Peer from "simple-peer";
+import io from "socket.io-client";
 
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect("http://localhost:5000");
 
 const App = () => {
-  const [me, setMe] = useState("")
-  const [stream, setStream] = useState()
-  const [receivingCall, setReceivingCall] = useState(false)
-  const [caller, setCaller] = useState("")
-  const [callerSignal, setCallerSignal] = useState()
-  const [callAccepted, setCallAccepted] = useState(false)
-  const [idToCall, setIdToCall] = useState("")
-  const [callEnded, setCallEnded] = useState(false)
-  const [name, setName] = useState("")
-  const myVideo = useRef()
-  const userVideo = useRef()
-  const connectionRef = useRef()
+  const [me, setMe] = useState("");
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
 
   useEffect(() => {
-    navigator
-      .mediaDevices
+    navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then((stream) => {
-        setStream(stream)
-        myVideo.current.srcObject = stream
-      })
+        setStream(stream);
+        myVideo.current.srcObject = stream;
+      });
 
     socket.on("me", (id) => {
-      setMe(id)
-    })
+      setMe(id);
+    });
 
     socket.on("callUser", (data) => {
-      setReceivingCall(true)
-      setCaller(data.from)
-      setName(data.name)
-      setCallerSignal(data.signal)
-    })
-  }, [])
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
+  }, []);
 
   const callUser = (id) => {
     const peer = new Peer({
       initiator: true,
       trickle: false,
-      stream: stream
-    })
+      stream: stream,
+    });
     peer.on("signal", (data) => {
       socket.emit("callUser", {
         userToCall: id,
         signalData: data,
         from: me,
-        name: name
-      })
-    })
+        name: name,
+      });
+    });
     peer.on("stream", (stream) => {
-      userVideo.current.srcObject = stream
-    })
+      userVideo.current.srcObject = stream;
+    });
     socket.on("callAccepted", (signal) => {
-      setCallAccepted(true)
-      peer.signal(signal)
-    })
-    connectionRef.current = peer
-  }
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+    connectionRef.current = peer;
+  };
 
   const answerCall = () => {
-    setCallAccepted(true)
+    setCallAccepted(true);
     const peer = new Peer({
       initiator: false,
       trickle: false,
-      stream: stream
-    })
+      stream: stream,
+    });
     peer.on("signal", (data) => {
-      socket.emit("answerCall", { signal: data, to: caller })
-    })
+      socket.emit("answerCall", { signal: data, to: caller });
+    });
     peer.on("stream", (stream) => {
-      userVideo.current.srcObject = stream
-    })
+      userVideo.current.srcObject = stream;
+    });
 
-    peer.signal(callerSignal)
-    connectionRef.current = peer
-  }
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+  };
 
   const leaveCall = () => {
-    setCallEnded(true)
-    connectionRef.current.destroy()
-  }
+    setCallEnded(true);
+    connectionRef.current.destroy();
+  };
   return (
     <>
       <Grid container>
         <Grid item xs="6">
-          {stream && <video playsInline muted ref={myVideo} autoPlay style={{ height: '100vh', width: '50vw' }} />}
+          {stream && (
+            <video
+              playsInline
+              muted
+              ref={myVideo}
+              autoPlay
+              style={{ height: "100vh", width: "50vw" }}
+            />
+          )}
         </Grid>
         <Grid item xs="6">
-          {callAccepted && !callEnded ?
-            <video playsInline ref={userVideo} autoPlay style={{ height: '100vh', width: '50vw' }} /> : null}
+          {callAccepted && !callEnded ? (
+            <video
+              playsInline
+              ref={userVideo}
+              autoPlay
+              style={{ height: "100vh", width: "50vw" }}
+            />
+          ) : null}
         </Grid>
       </Grid>
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
           paddingLeft: 10,
           paddingRight: 10,
           paddingTop: 10,
           paddingBottom: 10,
           top: 0,
           right: 0,
-          position: 'fixed'
+          position: "fixed",
         }}
       >
         <CopyToClipboard text={me}>
@@ -125,20 +138,24 @@ const App = () => {
       </div>
       <Card
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
           paddingLeft: 20,
           paddingRight: 20,
           paddingTop: 15,
           paddingBottom: 15,
           bottom: 0,
-          position: 'fixed'
+          position: "fixed",
         }}
       >
         <div className="call-button">
           {callAccepted && !callEnded ? (
-            <IconButton variant="contained" color="secondary" onClick={leaveCall}>
+            <IconButton
+              variant="contained"
+              color="secondary"
+              onClick={leaveCall}
+            >
               <PhoneIcon fontSize="medium" />
             </IconButton>
           ) : (
@@ -157,7 +174,11 @@ const App = () => {
                 style={{ marginRight: 20 }}
                 onChange={(e) => setIdToCall(e.target.value)}
               />
-              <IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
+              <IconButton
+                color="primary"
+                aria-label="call"
+                onClick={() => callUser(idToCall)}
+              >
                 <PhoneIcon fontSize="medium" />
               </IconButton>
             </>
@@ -167,34 +188,40 @@ const App = () => {
       {receivingCall && !callAccepted ? (
         <Card
           style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
             paddingLeft: 20,
             paddingRight: 20,
             paddingTop: 10,
             paddingBottom: 10,
             right: 0,
             bottom: 0,
-            position: 'fixed'
+            position: "fixed",
           }}
         >
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <h3>{name}</h3>
             <p style={{ marginBottom: 10 }}>is calling...</p>
-            <IconButton color="primary" variant="contained" onClick={answerCall}>
+            <IconButton
+              color="primary"
+              variant="contained"
+              onClick={answerCall}
+            >
               <PhoneIcon fontSize="medium" />
             </IconButton>
           </div>
         </Card>
       ) : null}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
